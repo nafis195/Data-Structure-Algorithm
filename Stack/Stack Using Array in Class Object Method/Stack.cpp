d9c6299691c75393d6817c18c95c775dc0f71b7e
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-11-21 18:28:28 -0800

    Add files via upload

---
+++ Stack/Stack Using Array in Class Object Method/Stack.cpp
@@ -0,0 +1,71 @@
+// Bismillahir Rahmanir Rahim
+
+// Stack Using Array in Class Object Method
+
+// Stack.cpp
+
+#include"Stack.h"
+
+// constructor
+Stack::Stack()
+{
+	top = -1;
+}
+
+// destructor
+Stack::~Stack()
+{
+
+}
+
+// push operation
+void Stack::push(int item)
+{
+	top++;
+	A[top] = item;
+}
+
+// pop operation
+void Stack::pop(int & item)
+{
+	item = A[top];
+	top--;
+}
+
+// isEmpty function
+bool Stack::isEmpty()
+{
+	if (top == -1)
+		return true;
+	else
+		return false;
+
+	// return (top == -1)  this could be used instead of 4 above lines.
+}
+
+// isFull function
+bool Stack::isFull()
+{
+	if (top == SIZE - 1)
+		return true;
+	else
+		return false;
+
+	// retrun (top == SIZE - 1)  this could be used instead of 4 above lines. 
+}
+
+// displayStack function
+void Stack::displayStack()
+{
+	cout << "Stack: ";
+	for (int i = 0; i <= SIZE; i++)
+	{
+		cout << A[i] << ' ';
+	}
+}
+
+// makeEmpty function
+void Stack::makeEmpty()
+{
+	top = -1;
+}
---
+++ Stack/Stack Using Array in Class Object Method/Stack.h
@@ -0,0 +1,32 @@
+// Bismillahir Rahmanir Rahim
+
+// Stack Using Array in Class Object Method
+
+// Stack.h
+
+#pragma once
+#include<iostream>
+using namespace std;
+
+const int SIZE = 10;
+
+#ifndef STACK_H
+#define STACK_H
+
+class Stack
+{
+private:
+	int top;
+	int A[SIZE];
+public:
+	Stack();
+	~Stack();
+	void push(int item);
+	void pop(int & item);
+	bool isEmpty();
+	bool isFull();
+	void displayStack();
+	void makeEmpty();
+};
+
+#endif
