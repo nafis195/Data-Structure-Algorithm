596b8a543779a8ef3b7b6ae356274d5315561eb3
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-11-21 18:29:31 -0800

    Add files via upload

--- Stack/Stack Using Array in Class Object Method/Main.cpp
+++ Stack/Stack Using Array in Class Object Method/Main.cpp
@@ -1,1 +1,32 @@
+// Bismillahir Rahmanir Rahim
 
+// Stack Using Array in Class Object Method
+
+// Main.cpp
+
+#include"Stack.h"
+
+int main()
+{
+	Stack stack;
+	int number = 1;
+
+	while (!stack.isFull())
+	{
+		stack.push(number);
+		number++;
+	}
+	cout << "Stack is full" << endl;
+
+	cout << "Now popping off the stack." << endl;
+
+	while (!stack.isEmpty())
+	{
+		stack.pop(number);
+		cout << number << endl;
+	}
+	cout << "Stack is empty." << endl;
+
+	system("pause");
+	return 0;
+}
