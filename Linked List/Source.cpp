2a16cc5ff8b5570022a3b70ea7c9cb1d0aefbf0a
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-11-08 03:26:35 -0800

    Add files via upload

--- Linked List/Source.cpp
+++ Linked List/Source.cpp
@@ -104,6 +104,98 @@ void recursive_display_from_last_to_first(Node* root)
 }
 
 
+//============Counting the Nodes=========================
+void counting_the_node(Node* root)
+{
+	Node* temp = new Node();
+	temp = head;
+	int count = 0;
+
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+
+	cout << count;
+	count = 0;
+}
+
+
+//================Sum of the Node Elements===============
+void Sum(Node* root)
+{
+	int sum = 0;
+	Node* temp = new Node();
+	temp = head;
+
+	while (temp != NULL)
+	{
+		sum = sum + temp->data;
+		temp = temp->next;
+	}
+
+	cout << sum;
+	sum = 0;
+}
+
+
+//================Max Element=======================
+int max_element(Node* root)
+{
+	int max = root->data;
+	Node* temp = new Node();
+	temp = root;
+
+	while (temp != NULL)
+	{
+		if (temp->data > max)
+			max = temp->data;
+		else
+			temp = temp->next;
+	}
+
+	return max;
+}
+
+
+//================Min Element=======================
+int min_element(Node* root)
+{
+	int min = root->data;
+	Node* temp = new Node();
+	temp = root;
+
+	while (temp != NULL)
+	{
+		if (temp->data < min)
+			min = temp->data;
+		else
+			temp = temp->next;
+	}
+
+	return min;
+}
+
+
+//===================Searching FUnction================
+bool Search(Node* root, int value)
+{
+	Node* temp = new Node();
+	temp = head;
+
+	while (temp != NULL)
+	{
+		if (temp->data == value)
+			return true;
+		else
+			temp = temp->next;
+	}
+
+	return false;
+}
+
+
 //==================Main Function=======================
 int main()
 {
@@ -116,8 +208,23 @@ int main()
 	recursive_display_from_first_to_last(head);
 	cout << endl;
 
-	recursive_display_from_last_to_first(head);
-	cout << endl;
+	//recursive_display_from_last_to_first(head);
+	//cout << endl;
+
+	//counting_the_node(head);
+	//cout << endl;
+
+	//Sum(head);
+	//cout << endl;
+
+	/*cout << max_element(head) << endl;*/
+
+	/*cout << min_element(head) << endl;*/
+
+	if(Search(head, 160))
+		cout << "Item is in the list" << endl;
+	else
+		cout << "Item is not in the list" << endl;
 
 	return 0;
 }
